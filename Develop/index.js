8c8fe525fc80ad58dc2dd93f418bdde373b85f9d
Noe Rios <noerios@gmail.com>
2020-12-13 13:32:59 -0600

    adding inquire

--- Develop/index.js
+++ Develop/index.js
@@ -1,36 +1,62 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+let createReadMe = ({ name, description, installation, credits, license }) => {
+    return //readmeFileContent
+}
+
 // array of questions for user
 const questions = [
     {   type: "input",
-        prompt: "What is the name of your application?",
-        section: "project name"
-    },
-
-    {   type: "input",
-        prompt: "What is a description of your application?",
-        section: "description"
+        message: "What is the name of your application?",
+        name: "name"
     },
     {   type: "input",
-        prompt: "How is the application installed?",
-        section: "installation" 
+        message: "What is a description of your application?",
+        name: "description"
     },
     {   type: "input",
-        prompt: "Who are your collaborators?",
-        section: "credits" 
+        message: "How is the application installed?",
+        name: "installation" 
+    },
+    {   type: "input",
+        message: "Who are your collaborators?",
+        name: "credits" 
     },
     {   type: "list",
-        prompt: "Which license?",
+        message: "Which license?",
         choices: ["GPL", "Apache", "Ms-PL", "MIT", "Other"],
-        section: "license"}
+        name: "license"}
 ];
 
-// function to write README file
-function writeToFile(fileName, data) {
-}
+inquirer
+  .prompt(questions)
+  .then(answers => {
+    console.log(answers);
+
+
+    // function to write README file - "data" will pass through the 
+    
+    const template = createReadMe(answers);
+
+    fs.writeFile('README.md', template, (err) => {
+        if (err) throw err;
+        console.log('The file has been saved!');
+    });
+    
+
+
+  })
+  .catch(error => {
+      console.log(error);
+  });
+
+
 
 // function to initialize program
-function init() {
+//function init() {
 
-}
+//}
 
 // function call to initialize program
-init();
+//init();

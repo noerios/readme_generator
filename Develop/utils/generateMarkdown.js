673b1eb9a91a0153616f4d3bf399b7b755da3810
Noe Rios <noerios@gmail.com>
2020-12-14 18:18:46 -0600

    saving

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,9 +1,13 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `#Project Name: ${data.name},
+    
   #Project Description: ${data.description},
+  
   #Installation: ${data.installation},
+  
   #Credits:  ${data.credits},
+  
   #License: ${data.license}
 
 `;
